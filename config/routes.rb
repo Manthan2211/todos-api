95249bae3f536707a8bde36164b403f2e6b71560
Manthan2211 <manthanandharia@gmail.com>
2018-03-23 16:59:38 +0530

    apply versioning and Serialization

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :todos do
+ 
+ 
+ scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+scope module: :v1, constraints: ApiVersion.new('v1', true) do
+  resources :todos do
     resources :items
   end
+end
   resources :users do
     collection do
       post 'login'
